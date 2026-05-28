348285a1d89c1707c4c09846f73029f032530d07
Akusha Kumaar <akushakumaar@gmail.com>
2026-05-28 03:42:02 +0000

    We are exporting this Next.js app as a static site and building it into

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.riaz.zenithbrowse',
+  appName: 'Zenith Browse',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
